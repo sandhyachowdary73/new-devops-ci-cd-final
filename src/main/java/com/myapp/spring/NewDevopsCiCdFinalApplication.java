ee9618f104dc143994f0579288f9342ac20f8d14
sandhyachowdary73 <sandhyachowdary73@gmail.com>
2022-02-14 17:56:27 +0530

    updated

---
+++ src/main/java/com/myapp/spring/NewDevopsCiCdFinalApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDevopsCiCdFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewDevopsCiCdFinalApplication.class, args);
+	}
+
+}
